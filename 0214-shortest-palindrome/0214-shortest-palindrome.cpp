19a6228bfebc93c14e12c48c46639aafe7e29c21
Shivyanshi Agnihotri <2K23.csiot2310794@gmail.com>
2026-03-22 14:52:07 +0530

    Time: 4 ms (54.06%), Space: 15.6 MB (67.37%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+
+        string temp = s + "#" + rev;
+        vector<int> lps(temp.size(), 0);
+
+        for(int i = 1; i < temp.size(); i++) {
+            int j = lps[i-1];
+            while(j > 0 && temp[i] != temp[j])
+                j = lps[j-1];
+
+            if(temp[i] == temp[j])
+                j++;
+
+            lps[i] = j;
+        }
+
+        return rev.substr(0, s.size() - lps.back()) + s;
+    }
+};
