092c9a130111c5c54bdd79a1e22e5156c995acb8
Shivyanshi Agnihotri <2K23.csiot2310794@gmail.com>
2026-07-25 14:13:31 +0530

    Time: 4 ms (99.45%), Space: 53.7 MB (59.14%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        int total = 1 << n;
+
+        for (int i = 0; i < total; i++) {
+            ans.add(i ^ (i >> 1));
+        }
+
+        return ans;
+    }
+}
